18edb7f6f1887bf6815c6c67e042caa8d91d8a78
Vats4065 <vatsal.r.lakhani2626@gmail.com>
2024-05-07 09:17:41 +0530

    my-app

--- pages/blog/index.jsx
+++ pages/blog/index.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
 
 function index() {
   return (
-    <div><h1>blog</h1></div>
-  )
+    <>
+      <nav className="">
+        <li>
+          <a href="/">Home</a>
+        </li>
+        <li>
+          <a href="/service">Service</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/blog">Blog</a>
+        </li>
+      </nav>
+    </>
+  );
 }
 
-export default index
+export default index;
